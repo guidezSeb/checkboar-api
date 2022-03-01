67b6d7a9729be8c6578d8a9f16fc57cc99121dda
unknown <lepinettenico@gmail.com>
2022-03-01 14:41:31 +0100

    Schéma & appJS

--- Projet/src/components/App.js
+++ Projet/src/components/App.js
@@ -1,12 +1,15 @@
 import '../styles/App.css';
 import Navbar from './components/Navbar';
 import React from 'react';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <Navbar/>
+        <Switch></Switch>
+        </Router>
     </div>
   );
 }
