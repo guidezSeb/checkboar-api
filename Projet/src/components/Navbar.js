cb56db23d0c34f31615a415de09bdc5a11f4ea9d
unknown <lepinettenico@gmail.com>
2022-03-14 11:48:10 +0100

    fichier Button

--- Projet/src/components/Navbar.js
+++ Projet/src/components/Navbar.js
@@ -3,6 +3,10 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 function Navbar(){
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false)
+
     return(
         <>
             <nav classname="navbar">
@@ -10,9 +14,41 @@ function Navbar(){
                     <Link to='/' className='navbar-logo'>
                         Test <i className=''/>
                     </Link>
-                <div classname='menu-icon'>
-                    <></>
+                <div classname='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        Home
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/profile' className='nav-links' onClick={closeMobileMenu}>
+                        Profile
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/mangalist' className='nav-links' onClick={closeMobileMenu}>
+                        Manga List
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/animelist' className='nav-links' onClick={closeMobileMenu}>
+                        Anime List
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/booklist' className='nav-links' onClick={closeMobileMenu}>
+                        Book List
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/moovielist' className='nav-links' onClick={closeMobileMenu}>
+                        Movie List
+                    </Link>
+                </li>
+                </ul>
                 </div>
             </nav>
         </>
