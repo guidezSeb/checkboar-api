2e3ef68e07ab3fc31ff96f51d3f3a9e35ba908a4
guidezSeb <sebastien.guidez@lacatholille.fr>
2022-04-05 11:46:56 +0200

    test echo

--- API/symfonyApiCheckboar/public/index.php
+++ API/symfonyApiCheckboar/public/index.php
@@ -2,12 +2,8 @@
 
 use App\Kernel;
 
-// require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-    echo "hello world";
-    echo "hello world2";
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    // return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-    echo "hello world";
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
