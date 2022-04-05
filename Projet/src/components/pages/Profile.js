b40d473b7562f174c41bb4cdf6a9daeeab8d7d2e
unknown <lepinettenico@gmail.com>
2022-04-05 14:47:49 +0200

    page profil et styles

--- Projet/src/components/pages/Profile.js
+++ Projet/src/components/pages/Profile.js
@@ -1,6 +1,59 @@
 import React from 'react';
 import '../../styles/App.css';
+import Footer from '../Footer';
+import '../../styles/Element.css';
 
 export default function Profile() {
-  return <h1 className='Profile'>Profil</h1>;
+  return(
+    <>
+       <div className='PageProfil'>
+       <h2 className='Titre-page-produit'>Information</h2>
+      <div className='tableau'>
+      <table>
+        <tr>
+            <th>Photo</th>
+            <th>Pseudo</th>
+            <th>E-Mail</th>
+        </tr>
+        <tr>
+          <td>image</td>
+          <td>Sebgdu59</td>
+          <td>Leonard.guidez@hotmail.fr</td>
+        </tr>
+      </table>
+      </div>
+      <div className='tableau-profil'>
+        <table>
+<td>
+      <table>
+        <tr>
+            <th>Films Favoris</th>
+        </tr>
+        <tr>
+          <td>Kill Bill</td>
+          <td>Kill Bill 2</td>
+          <td>Matrix 1</td>
+        </tr>
+      </table>
+      </td>
+      <td>
+      <table>
+        <tr>
+            <th>Mangas Favoris</th>
+        </tr>
+        <tr>
+          <td>Conan</td>
+          <td>Grand Blue</td>
+          <td>Eyeshield 21</td>
+        </tr>
+      </table>
+      </td>
+      </table>
+
+      </div>
+
+      </div>
+    <Footer/>
+    </>
+  );
 }
