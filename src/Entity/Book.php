ac4c91752e4b10a14d54bce54e4ae58a5defe48b
guidezSeb <sebastien.guidez@lacatholille.fr>
2022-04-11 11:21:38 +0200

    git heroku new folder

---
+++ src/Entity/Book.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'date')]
+    private $BookDateStart;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $BookDateEnd;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $BookChapterInProgress;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $BookChapterTotal;
+
+    #[ORM\Column(type: 'integer')]
+    private $BookId;
+
+    #[ORM\OneToOne(inversedBy: 'book', targetEntity: Element::class, cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private $element;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBookDateStart(): ?\DateTimeInterface
+    {
+        return $this->BookDateStart;
+    }
+
+    public function setBookDateStart(\DateTimeInterface $BookDateStart): self
+    {
+        $this->BookDateStart = $BookDateStart;
+
+        return $this;
+    }
+
+    public function getBookDateEnd(): ?\DateTimeInterface
+    {
+        return $this->BookDateEnd;
+    }
+
+    public function setBookDateEnd(?\DateTimeInterface $BookDateEnd): self
+    {
+        $this->BookDateEnd = $BookDateEnd;
+
+        return $this;
+    }
+
+    public function getBookChapterInProgress(): ?int
+    {
+        return $this->BookChapterInProgress;
+    }
+
+    public function setBookChapterInProgress(?int $BookChapterInProgress): self
+    {
+        $this->BookChapterInProgress = $BookChapterInProgress;
+
+        return $this;
+    }
+
+    public function getBookChapterTotal(): ?int
+    {
+        return $this->BookChapterTotal;
+    }
+
+    public function setBookChapterTotal(?int $BookChapterTotal): self
+    {
+        $this->BookChapterTotal = $BookChapterTotal;
+
+        return $this;
+    }
+
+    public function getBookId(): ?int
+    {
+        return $this->BookId;
+    }
+
+    public function setBookId(int $BookId): self
+    {
+        $this->BookId = $BookId;
+
+        return $this;
+    }
+
+    public function getElement(): ?Element
+    {
+        return $this->element;
+    }
+
+    public function setElement(Element $element): self
+    {
+        $this->element = $element;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Manga.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MangaRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+#[ORM\Entity(repositoryClass: MangaRepository::class)]
+#[ApiResource]
+class Manga
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $MangaDateStart;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $MangaDateEnd;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $MangaChapterTotal;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $MangaChapterInProgress;
+
+    #[ORM\Column(type: 'integer')]
+    private $MangaId;
+
+    #[ORM\OneToOne(inversedBy: 'manga', targetEntity: Element::class, cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private $element;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMangaDateStart(): ?\DateTimeInterface
+    {
+        return $this->MangaDateStart;
+    }
+
+    public function setMangaDateStart(?\DateTimeInterface $MangaDateStart): self
+    {
+        $this->MangaDateStart = $MangaDateStart;
+
+        return $this;
+    }
+
+    public function getMangaDateEnd(): ?\DateTimeInterface
+    {
+        return $this->MangaDateEnd;
+    }
+
+    public function setMangaDateEnd(?\DateTimeInterface $MangaDateEnd): self
+    {
+        $this->MangaDateEnd = $MangaDateEnd;
+
+        return $this;
+    }
+
+    public function getMangaChapterTotal(): ?int
+    {
+        return $this->MangaChapterTotal;
+    }
+
+    public function setMangaChapterTotal(?int $MangaChapterTotal): self
+    {
+        $this->MangaChapterTotal = $MangaChapterTotal;
+
+        return $this;
+    }
+
+    public function getMangaChapterInProgress(): ?int
+    {
+        return $this->MangaChapterInProgress;
+    }
+
+    public function setMangaChapterInProgress(?int $MangaChapterInProgress): self
+    {
+        $this->MangaChapterInProgress = $MangaChapterInProgress;
+
+        return $this;
+    }
+
+    public function getMangaId(): ?int
+    {
+        return $this->MangaId;
+    }
+
+    public function setMangaId(int $MangaId): self
+    {
+        $this->MangaId = $MangaId;
+
+        return $this;
+    }
+
+    public function getElement(): ?Element
+    {
+        return $this->element;
+    }
+
+    public function setElement(Element $element): self
+    {
+        $this->element = $element;
+
+        return $this;
+    }
+}
