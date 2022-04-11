59a0b41fa9fbb941c4eb68c5bfdaebb23f4c2db8
guidezSeb <sebastien.guidez@lacatholille.fr>
2022-04-11 10:59:00 +0200

    debug heroku

--- API/symfonyApiCheckboar/public/index.php
+++ API/symfonyApiCheckboar/public/index.php
@@ -2,7 +2,7 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once 'D:\M2III\project_master\Checkboar\API\symfonyApiCheckboar\vendor\autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
