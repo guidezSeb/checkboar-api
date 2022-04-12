10d731e35a0ea3597fba8a7f55388b14a89c38f5
unknown <lepinettenico@gmail.com>
2022-04-12 11:01:33 +0200

    page d'accueil

--- Projet/src/components/pages/Home.js
+++ Projet/src/components/pages/Home.js
@@ -5,6 +5,7 @@ import Footer from '../Footer';
 function Home() {
   return (
     <>
+    <div className='Home'>
     <div className='Home-tab-books'>
     <table>
         <tr>
@@ -18,8 +19,52 @@ function Home() {
           </tr>
           </table>
         </table>
+    </div>
 
-        
+    <div className='Home-tab-mangas'>
+    <table>
+        <tr>
+          <th>Manga in Progress</th>
+        </tr>
+        <table>
+          <tr>
+            <td><img style={{width:80, height:80}} alt="manga_image" src={require('../../Images/tmp/conan_1.jpg')}/></td>
+            <td><img style={{width:80, height:80}} alt="manga_image" src={require('../../Images/tmp/conan_1.jpg')}/></td>
+            <td><img style={{width:80, height:80}} alt="manga_image" src={require('../../Images/tmp/conan_1.jpg')}/></td>
+          </tr>
+          </table>
+        </table>
+    </div>
+
+    <div className='Home-tab-movies'>
+    <table>
+        <tr>
+          <th>Movie plan to watch</th>
+        </tr>
+        <table>
+          <tr>
+            <td><img style={{width:80, height:80}} alt="movie_image" src={require('../../Images/tmp/IFSLSR.jpg')}/></td>
+            <td><img style={{width:80, height:80}} alt="movie_image" src={require('../../Images/tmp/IFSLSR.jpg')}/></td>
+            <td><img style={{width:80, height:80}} alt="movie_image" src={require('../../Images/tmp/IFSLSR.jpg')}/></td>
+          </tr>
+          </table>
+        </table>
+    </div>
+
+    <div className='Home-tab-animes'>
+    <table>
+        <tr>
+          <th>Animes in Progress</th>
+        </tr>
+        <table>
+          <tr>
+            <td><img style={{width:80, height:80}} alt="anime_image" src={require('../../Images/tmp/Naruto_shipu.jpg')}/></td>
+            <td><img style={{width:80, height:80}} alt="anime_image" src={require('../../Images/tmp/Naruto_shipu.jpg')}/></td>
+            <td><img style={{width:80, height:80}} alt="anime_image" src={require('../../Images/tmp/Naruto_shipu.jpg')}/></td>
+          </tr>
+          </table>
+        </table>
+    </div>
     </div>
   <Footer />
   </>
