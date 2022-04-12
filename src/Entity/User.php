bb401df4199dad84c8012f5e3e4d0d8a42011387
guidezSeb <sebastien.guidez@lacatholille.fr>
2022-04-12 10:20:55 +0200

    add Apiressource

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
 
-@apiRessource();
+@ApiRessource();
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ApiResource]
 class User
