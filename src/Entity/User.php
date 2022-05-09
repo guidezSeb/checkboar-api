aaee495660d46b8224a0fdba5515d51092d9b81d
guidezSeb <sebastien.guidez@lacatholille.fr>
2022-05-09 13:41:35 +0200

    check api

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -43,7 +43,7 @@ class User
         $this->comments = new ArrayCollection();
         $this->userElements = new ArrayCollection();
     }
-
+ 
     public function getId(): ?int
     {
         return $this->id;
