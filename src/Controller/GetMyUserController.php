b804a8c5035999c1b38e8d08cd0e74ad8101197b
guidezSeb <sebastien.guidez@lacatholille.fr>
2022-05-30 11:26:39 +0200

    api myuser

--- src/Controller/GetMyUserController.php
+++ src/Controller/GetMyUserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\UserRepository;
 
 class GetMyUserController extends AbstractController
